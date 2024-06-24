1ce7e3414b26ba492c7c18887f6bd7fe65125026
Oskar <os.moreno@duocuc.cl>
2024-06-24 13:54:07 -0400

    se añaden vistas de registro y catalogo

--- mysite_PGY/tienda/views.py
+++ mysite_PGY/tienda/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from tienda.forms import RegistrationForm
+from .models import Bebidas
 
 # Create your views here.
 
-# Ex: tienda/inicio/
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# Ex: localhost:8000/tienda/inicio/
+def inicio(request):
+    return render(request,"tienda/inicio.html",{})
+
+def catalogo(request):
+    objetos = Bebidas.objects.all()
+    contexto = {"bebidas" : objetos}
+    return render(request, "tienda/catalogo.html", contexto)
+
+def registro(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/tienda/inicio')
+    else:
+        form = RegistrationForm()
+    
+    return render(request, 'tienda/registro.html', {'form': form})
