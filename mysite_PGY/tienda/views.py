2003833aaf0f33cec24e7ecf77b11651ee9f4eb3
Oskar <os.moreno@duocuc.cl>
2024-06-24 20:00:29 -0400

    reescritura

--- mysite_PGY/tienda/views.py
+++ mysite_PGY/tienda/views.py
@@ -25,3 +25,35 @@ def registro(request):
         form = RegistrationForm()
     
     return render(request, 'tienda/registro.html', {'form': form})
+
+def carrito(request):
+    carrito = request.session.get('carrito', [])
+    total = sum(item['precio'] * item['cantidad'] for item in carrito)
+    contexto = {
+        'carrito': carrito,
+        'total': total
+    }
+    return render(request, 'tienda/carroDeCompras.html', contexto)
+
+def agregar_al_carrito(request, id_bebida):
+    bebida = Bebidas.objects.get(id=id_bebida)
+    carrito = request.session.get('carrito', [])
+    
+    for item in carrito:
+        if item['id'] == bebida.id:
+            item['cantidad'] += 1
+            break
+    else:
+        carrito.append({
+            'id': bebida.id,
+            'nombre': bebida.nombre,
+            'precio': bebida.precio,
+            'cantidad': 1,
+            'imagen': bebida.imagen.url if bebida.imagen else None
+        })
+    
+    request.session['carrito'] = carrito
+    
+    if 'from_cart' in request.POST:
+        return redirect('/tienda/carrito')
+    return redirect('/tienda/catalogo')
